989ca903f6033d7387f39e93e5bcab9f04f7d0e6
Vladimir Macko <vladimir.v.macko@gmail.com>
2026-01-12 17:43:28 +0100

    Run experiments on delta8 encoding

--- c_benchmarking/run_tests.sh
+++ c_benchmarking/run_tests.sh
@@ -25,7 +25,7 @@ for ((i=0; i<${#Rs[@]}; i++)); do
 
     for algo in ${algos};
     do
-        for d in 1.0 0.9 0.8 0.7 0.6 0.5 0.4 0.3 0.2 0.1 0.05;
+        for d in 1.0 0.9 0.8 0.7 0.6 0.5 0.4 0.3 0.2 0.1 0.09 0.08 0.07 0.06 0.05 0.04 0.03 0.02 0.01;
         do
             partial_result_file=${base_results}/partial/${R}_${C}_${algo}_${d}.txt
             ./benchmark 16 $R $C 100 1000 1 0 $algo 47 $d > $partial_result_file
--- c_benchmarking/run_tests_fp16.sh
+++ c_benchmarking/run_tests_fp16.sh
@@ -1,1 +1,1 @@
-bash run_tests.sh 16 "2 3 7"
+bash run_tests.sh 16 "7 3 2"
